3ad9441a2240a581c4757c4c01d166810c34bde6
Angela <angelakulola4@gmail.com>
2023-09-04 17:06:45 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
+import DoctorAppointments from "./components/Doctor/DoctorAppointments";
+import PatientAppointments from "./components/Patient/PatientAppointments";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/doctor/:id" element={<DoctorAppointments/>} />
+        <Route path="/patient/:id" element={<PatientAppointments/>} />
       </Routes>
     </div>
   );
