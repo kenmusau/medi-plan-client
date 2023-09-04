0e7bc282f5fb7ec4d76099e77b1c48f7d9301576
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-04 23:13:50 +0300

    add onsubmit

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 function LogInForm() {
-  return (
-    <div>
-      <form>
-        <label htmlFor=""></label>
-      </form>
-    </div>
-  );
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  function onSubmit(data) {
+    console.log("Form Submit", data);
+  }
+
+  return <div className="hook_form"></div>;
 }
 
 export default LogInForm;
