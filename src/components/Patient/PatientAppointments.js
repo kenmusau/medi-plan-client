7afd8150f29d038a4215c3b3b80c58b7df4c2c36
Angela <angelakulola4@gmail.com>
2023-09-04 22:04:26 +0300

    Created form for patient

--- src/components/Patient/PatientAppointments.js
+++ src/components/Patient/PatientAppointments.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 
 function PatientAppointments() {
   const [appointments, setAppointments] = useState([]);
-
+  const [newAppointment, setNewAppointment] = useState({
+    date: "",
+    time: "",
+    description: "",
+  });
   useEffect(() => {
     fetch("/api/patientAppointments")
       .then((response) => response.json())
@@ -14,9 +18,69 @@ function PatientAppointments() {
       });
   }, []);
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewAppointment({ ...newAppointment, [name]: value });
+  };
+
+  const handleCreateAppointment = (e) => {
+    e.preventDefault();
+    fetch("API", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newAppointment),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            setAppointments([...appointments, data]);
+            setNewAppointment({
+                date: "",
+                time: "",
+                description: "",
+            });
+        })
+        .catch((error) => console.error("Error creating appointment:", error));
+        };
+    
+
+
   return (
     <div>
       <h1>Your Appointments</h1>
+      <form onSubmit={handleCreateAppointment}>
+        <div>
+          <label>Date:</label>
+          <input
+            type="date"
+            name="date"
+            value={newAppointment.date}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Time:</label>
+          <input
+            type="time"
+            name="time"
+            value={newAppointment.time}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label>Description:</label>
+          <textarea
+            name="description"
+            value={newAppointment.description}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Create Appointment</button>
+      </form>
       <ul>
         {appointments.map((appointment) => (
           <li key={appointment.id}>
