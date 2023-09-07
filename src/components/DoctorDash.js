56403572696d582f296facdce7ddc84ee70bc023
Kennedy Musau <kennedy.musauu@gmail.com>
2023-09-07 17:14:12 +0300

    update doc css

--- src/components/DoctorDash.js
+++ src/components/DoctorDash.js
@@ -1,8 +1,8 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import moment from "moment";
 import { baseUrl } from "../utlis";
-
+import "./doctorDash.css";
 
 function DoctorDash({ onSetLoggedUser }) {
   const { state } = useLocation();
@@ -18,7 +18,7 @@ function DoctorDash({ onSetLoggedUser }) {
     });
   }
   const doc = state.loggedInDoc;
-  
+
   console.log(doc);
 
   useEffect(() => {
@@ -28,28 +28,37 @@ function DoctorDash({ onSetLoggedUser }) {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
   return (
-    <div>
-      <h1>DoctorDash</h1>
-      <p>welcome {doc.full_name}</p>
+    <div className="docDash_app">
+      <h1 className="doc_title">DoctorDash</h1>
+      <div className="doc_header">
+        <h1>Welcome Dr {doc.full_name}</h1>
+        <button className="btn btn_logout" onClick={handleLogoutClick}>
+          Log Out
+        </button>
+      </div>
       <h2>Doctor's Appointments</h2>
       <ul>
-        {patients.length > 0 && patients.map(patient => (
-          <li key={patient.id}>
-            <h3>Patient: {patient.full_name}</h3>
-            <p>Date of Birth: {moment(patient.dob).format('MMMM Do YYYY')}</p>
-            <p>Gender: {patient.gender}</p>
-            <h4>Appointments</h4>
-            {patient.appointments.map(appointment => (
-              <ul key={appointment.id}>
-                <p>Date: {moment(appointment.date_time).format('MMMM Do YYYY, h:mm:ss a')}</p>
-                <p>Description: {appointment.description}</p>
+        {patients.length > 0 &&
+          patients.map((patient) => (
+            <li key={patient.id} className="appointment_list">
+              <h3>Patient: {patient.full_name}</h3>
+              <p>Date of Birth: {moment(patient.dob).format("MMMM Do YYYY")}</p>
+              <p>Gender: {patient.gender}</p>
+              <h4>Appointments</h4>
+              {patient.appointments.map((appointment) => (
+                <ul key={appointment.id} className="patient_appointment">
+                  <p>
+                    Date:{" "}
+                    {moment(appointment.date_time).format(
+                      "MMMM Do YYYY, h:mm:ss a"
+                    )}
+                  </p>
+                  <p>Description: {appointment.description}</p>
                 </ul>
-            )
-              )}
-          </li>
-        ))}
+              ))}
+            </li>
+          ))}
       </ul>
-      <button onClick={handleLogoutClick}>Log Out</button>
     </div>
   );
 }
